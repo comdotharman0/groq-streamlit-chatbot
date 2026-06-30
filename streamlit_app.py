debc9cc9a4b9c787d4453ccce9d2076584161117
Harman Singh <harman.sys.dev@protonmail.com>
2026-06-30 14:36:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,19 @@ st.header("Groq Streamlit Chatbot 🤖")
 client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
-
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain the importance of fast language models",
-        }
-    ],
-    model="llama-3.3-70b-versatile",
-)
+def generate_text(question,max_tokens):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+        max_tokens=max_tokens,
+    )
+chat_input = st.text_area("Ask Anything")
+chat_submit_btn = st.button("Submit")
 
 st.write(chat_completion.choices[0].message.content)
 
