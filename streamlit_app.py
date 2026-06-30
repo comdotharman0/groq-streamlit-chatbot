c356e66a54ea4c545c8d521111b338530744f45c
Harman Singh <harman.sys.dev@protonmail.com>
2026-06-30 16:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,29 +6,42 @@ class MyChatBot:
         self.name= name
         st.title(name)
         st.header(name)
+        self.chat_input = st.text_area("Ask Anything")
+        self.chat_submit_btn = st.button("Submit")
         self.client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
         )
         self.messages = []
         self.max_tokens=max_tokens
         self.model = model
-    def generate_text(question):
-        chat_completion = client.chat.completions.create(
-        messages=[
-            {
+    def generate_text(self,question):
+        self.messages.append({
                 "role": "user",
                 "content": question,
-            }
+        });
+        chat_completion = client.chat.completions.create(
+        messages=[
+            self.messages[-1]
         ],
         model=self.model,
         max_tokens=self.max_tokens,
     )
-        return chat_completion.choices[0].message.content
-
-chat_input = st.text_area("Ask Anything")
-chat_submit_btn = st.button("Submit")
-if chat_submit_btn:
-    st.write(generate_text(chat_input,150))
+        response = chat_completion.choices[0].message.content
+        self.messages.append({
+                "role": "bot",
+                "content": response,
+        })
+        return self.messages[-1]["content"]
+    def run(self):
+        if self.chat_submit_btn and self.chat_input != "":
+            self.messages.append({
+                "role": "user",
+                "content": self.chat_input,
+            })
+            self.generate_text(self.chat_input)
+        for i in self.messages:
+            self.write(i.content)
+            
     
 
 def main():
@@ -36,6 +49,7 @@ def main():
         bot = MyChatBot("Groq Streamlit ChatBot",
                     150,
                     "llama-3.3-70b-versatile")
+        bot.run()
     except RateLimitError as rle:
         st.write("Got "+str(rle))
     except APIConnectionError as ace:
