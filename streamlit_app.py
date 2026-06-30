6327c67a0562c6d4989dc2e7259e982394423b47
Harman Singh <harman.sys.dev@protonmail.com>
2026-06-30 15:48:53 +0530

    Enhance MyChatBot with error handling
    Added error handling for RateLimitError, APIConnectionError, and AIStatusError in MyChatBot initialization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from groq import Groq
+from groq import Groq,RateLimitError,APIConnectionError, AIStatusError
 class MyChatBot:
     def __init__(self,name,max_tokens,model):
         self.name= name
@@ -20,7 +20,7 @@ class MyChatBot:
                 "content": question,
             }
         ],
-        model=,
+        model=self.model,
         max_tokens=self.max_tokens,
     )
     return chat_completion.choices[0].message.content
@@ -34,9 +34,20 @@ if chat_submit_btn:
     
 
 def main():
-    bot = MyChatBot("Groq Streamlit ChatBot",
+    try:
+        bot = MyChatBot("Groq Streamlit ChatBot",
                     150,
                     "llama-3.3-70b-versatile")
-                    
+    except RateLimitError as rle:
+        st.write("Got "+str(rle))
+    except APIConnectionError as ace:
+        st.write("Got "+str(ace))
+    except AIStatusError as ase:
+        st.write("Got "+str(ase))
+    finally:
+        st.write("That's All!")
+if __name__=="__main__":
+    main()
+    
 
 #st.write(pd.DataFrame({"A": [1,2,3],"B":[4,5,6]}))
