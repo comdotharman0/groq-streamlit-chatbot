8dfba91aca35e23972ae9110925849dbbbd00cd6
Harman Singh <harman.sys.dev@protonmail.com>
2026-06-30 13:55:55 +0530

    Integrate Groq API for chatbot functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+from groq import Groq
+st.title("Groq Streamlit Chatbot")
+St.header("Groq Streamlit Chatbot 🤖")
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"],
 )
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+st.write(chat_completion.choices[0].message.content)
+
