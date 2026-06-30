9e326aba18ec06c84761c69cdb4bf087ff814acb
Harman Singh <harman.sys.dev@protonmail.com>
2026-06-30 14:25:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 from groq import Groq
 st.title("Groq Streamlit Chatbot")
-St.header("Groq Streamlit Chatbot 🤖")
-"""client = Groq(
+st.header("Groq Streamlit Chatbot 🤖")
+client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
 
@@ -18,5 +18,5 @@ chat_completion = client.chat.completions.create(
 )
 
 st.write(chat_completion.choices[0].message.content)
-"""
+
 st.write(pd.DataFrame({"A": [1,2,3],"B":[4,5,6]}))
